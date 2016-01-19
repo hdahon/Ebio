01a795ff3de99f0160e2236d5159586eb498c2d1
EK <elmahdi.korfed@gmail.com>
2016-01-19 20:43:41 +0100

    updates project

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'dataEbio',
+            'database'  => 'dataebio',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8',
