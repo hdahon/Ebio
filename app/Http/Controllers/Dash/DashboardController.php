22405344179a9cb6d7f0e6918127bde687719a61
Kévin <progkeving@gmail.com>
2016-02-21 21:30:28 +0100

    gestion problèmes de login autre que amapien et producteur

--- app/Http/Controllers/Dash/DashboardController.php
+++ app/Http/Controllers/Dash/DashboardController.php
@@ -26,40 +26,15 @@ class DashboardController extends Controller
             $niveau = session('role');
         } elseif (!Auth::guest() and session()->has('role') ) {
             $role = Auth::user()->roleamapien_id;
-            $niveau = session('role');
-        
+            $niveau = session('role'); 
         } else {
             $niveau = 0;
         }
         
-        if ($niveau == 1) {
+        if ($niveau) {
             //echo '1';
             $data = array('user' => Auth::user(),'role' => RoleAmapien::find($role)->nomRole,'date' => date('Y-m-d'));
             return view('dashboard/amapien_accueil')->with($data);
-
-        } elseif( $niveau == 2 ) {
-            //echo '2';
-            $data = array('prod' => Auth::user(),
-            'date' => date('Y-m-d'),
-            'role' => RoleAmapien::find($role)->nomRole);
-            return view('dashboard/producteur')->with($data);
-
-        } elseif( $niveau == 3) {
-            //echo '3';
-            $data = array('name' => Auth::user()->nom,
-            'date' => date('Y-m-d'),
-            'role' => RoleAmapien::find($role)->nomRole);
-            return view('dashboard/referent')->with($data);
-        } elseif( $niveau == 4) {
-            //echo '4';
-            $data = array('user' => Auth::user(),'role' => RoleAmapien::find($role)->nomRole,'date' => date('Y-m-d'));
-            return view('dashboard/amapien_accueil')->with($data);
-
-        } elseif( $niveau == 5 ) {
-            // echo '5';
-            $data = array('name' => Auth::user()->nom, 'date' => date('Y-m-d'), 'role' => Auth::user()->roleamapien_id);
-            return view('dashboard/homeAdmin')->with($data);
-
         } else {
             $data = array('name' => Auth::user()->nom,
             'date' => date('Y-m-d'));
