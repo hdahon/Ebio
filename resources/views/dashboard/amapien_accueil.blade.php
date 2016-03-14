86ea1e4404bf60f9154502cc4c41c92c2eba59d6
EK <elmahdi.korfed@gmail.com>
2016-03-14 22:56:40 +0100

    Profil ADMIN : le coinjoint et la modification du profil n'apparaissent plus comme demandé

--- resources/views/dashboard/amapien_accueil.blade.php
+++ resources/views/dashboard/amapien_accueil.blade.php
@@ -1,10 +1,18 @@
 @extends('template')
 @section('content')
+<?php  
 
+    if(Session::has('role') and Auth::check()){
+        $niveau = session('role');
+    } else {
+        $niveau = 0;
+    }
+
+?>
         <div class="row">
             <div class="col-md-11 col-md-offset-1">
                         <h1>Bienvenue, {{$user->nom}} sur la page amapien </h1>
-                        
+                  
                         Voici vos informations: 
                         <ul>
                         	<li>Votre nom : {{$user->nom}}</li>
@@ -14,6 +22,7 @@
                         	<li>Votre rôle au sein de l'association : {{$role}} </li>
                         </ul>
 
+                        @if ($niveau!=5)
                         <h2>Voici les informations de votre conjoint: </h2>
                         <ul>
                         	<li>Nom  de votre conjoint: {{$user->nomCAdherant}}</li>
@@ -22,6 +31,7 @@
                         	<li>Numéro de téléphone de votre conjoint : {{$user->telCAdherant}}</li> 
                         </ul>
                         {!! link_to_route('amap_change_data','Changer ses informations',array(),array('class'=>'waves-effect waves-light btn')) !!}
+                        @endif
             </div>
         </div>
 
