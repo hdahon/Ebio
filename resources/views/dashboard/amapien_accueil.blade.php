b23cb110064379b1b0a1e3587a66d8858ab8729d
EK <elmahdi.korfed@gmail.com>
2016-03-15 23:30:52 +0100

    msg selon le profil pour amapien_accueil

--- resources/views/dashboard/amapien_accueil.blade.php
+++ resources/views/dashboard/amapien_accueil.blade.php
@@ -1,9 +1,42 @@
 @extends('template')
 @section('content')
 
+<?php  
+
+    if(Session::has('role') and Auth::check()){
+        $niveau = session('role');
+    } else {
+        $niveau = 0;
+    }
+
+    switch ($niveau){
+        case 1:{
+            $role = "amapien";
+            break;
+        }
+        case 2:{
+            $role = "producteur";
+            break;
+        }
+        case 3:{
+            $role = "référent";
+            break;
+        }
+        case 4:{
+            $role = "référent plus";
+            break;
+        }
+        case 5:{
+            $role = "admin";
+            break;
+        }
+    }
+
+?>
+
         <div class="row">
             <div class="col-md-11 col-md-offset-1">
-                        <h1>Bienvenue, {{$user->prenom}} {{$user->nom}}</h1>
+                        <h1>Bienvenue {{$user->prenom}} {{$user->nom}} sur la page d'accueil <?php echo $role; ?></h1>
                         
                         Voici vos informations: 
                         <ul>
@@ -14,6 +47,7 @@
                         	<li><b>Votre rôle au sein de l'association : </b>{{$role}} </li>
                         </ul>
 
+                        @if(session('role') <5)
                         <h2>Voici les informations de votre conjoint: </h2>
                         <ul>
                         	<li>Nom  de votre conjoint: {{$user->nomCAdherant}}</li>
@@ -21,7 +55,6 @@
                         	<li>Mail de votre conjoint: {{$user->emailCAdherant}}</li>
                         	<li>Numéro de téléphone de votre conjoint : {{$user->telCAdherant}}</li> 
                         </ul>
-                        @if(session('role') <5)
                         {!! link_to_route('amap_change_data','Changer vos informations de compte',array(),array('class'=>'waves-effect waves-light btn')) !!}
                         @endif     
        </div>
