62a5480f6955a77b123bbc918959b0912ce4c23b
EK <elmahdi.korfed@gmail.com>
2016-03-20 15:02:51 +0100

    onglet onglet admin avec parametre

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,96 +1,70 @@
 
 <li><a href="{{ url('list-users') }}">Utilisateurs</a></li>
-<<<<<<< HEAD
 <li class="dropdown">
-   <a  data-toggle="dropdown" class="dropdown-toggle" href="">Paiements<b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('liste-paiement') }}">Historique</a></li>
-        <li><a href="{{ url('create-paiement') }}">Ajouter un paiement</a></li>   
-    </ul>
+<a href="{{ url('list-contratsClients') }}" role="button" aria-haspopup="true" aria-expanded="false">Contrats Amapiens  </span></a>
+</li> 
+<!--li><a href="{{ url('list-roles') }}">Rôles</a></li>
+<li><a href="{{ url('list-periodicites') }}">Periodicité</a></li-->
+<li class="dropdown">
+<a  data-toggle="dropdown" class="dropdown-toggle" href="">Paiements<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('liste-paiement') }}">Historique</a></li>
+<li><a href="{{ url('create-paiement') }}">Ajouter un paiement</a></li>   
+</ul>
 </li> 
 <li class="dropdown"> 
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Livraisons<span class="caret"></span></a>
-    <ul class="dropdown-menu">
-        <li>Report</li>
-        <li><a href="{{ url('list-livraisons') }}">Livraisons</a></li>
-       
-    </ul>
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Livraisons<span class="caret"></span></a>
+<ul class="dropdown-menu">
+<li>Report</li>
+<li><a href="{{ url('list-livraisons') }}">Livraisons</a></li>
+<!--li> { !! link_to_action('Producteur\Livraison\LivraisonController@getLivraisonsAmap','Vos Livraisons',array(Auth::user()->id)) !!}</li-->
+</ul>
 </li>
 <li class="dropdown">
-    <a  data-toggle="dropdown" class="dropdown-toggle" href="">Produits<b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('liste-produit') }}">Liste des produits</a></li>
-        <li><a href="{{ url('liste-categorie') }}">Listes des catégories</a></li>
-        <li><a href="{{ url('create-produit') }}">Ajouter un produit</a></li> 
-        <li><a href="{{ url('create-categorie') }}">Ajouter une catégorie</a></li>   
-    </ul>
+<a  data-toggle="dropdown" class="dropdown-toggle" href="">Produits<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('liste-produit') }}">Liste des produits</a></li>
+<li><a href="{{ url('categorie-liste') }}">Listes des catégories</a></li>
+<li><a href="{{ url('create-produit') }}">Ajouter un produit</a></li> 
+<li><a href="{{ url('create-categorie') }}">Ajouter une catégorie</a></li>   
+</ul>
 </li>
-<li class="dropdown">
-    <a href="{{ url('list-contratsClients') }}" role="button" aria-haspopup="true" aria-expanded="false">Contrats Amapiens  </span></a>
-=======
-<!--li><a href="{{ url('list-roles') }}">Rôles</a></li>
-    <li><a href="{{ url('list-periodicites') }}">Periodicité</a></li-->
-        <li class="dropdown">
-         <a  data-toggle="dropdown" class="dropdown-toggle" href="">Paiements<b class="caret"></b></a>
-         <ul class="dropdown-menu">
-            <li><a href="{{ url('liste-paiement') }}">Historique</a></li>
-            <li><a href="{{ url('create-paiement') }}">Ajouter un paiement</a></li>   
-        </ul>
-    </li> 
-    <li class="dropdown"> 
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Livraisons<span class="caret"></span></a>
-        <ul class="dropdown-menu">
-            <li>Report</li>
-            <li><a href="{{ url('list-livraisons') }}">Livraisons</a></li>
-            <!--li> {!! link_to_action('Producteur\Livraison\LivraisonController@getLivraisonsAmap','Vos Livraisons',array(Auth::user()->id)) !!}</li-->
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a  data-toggle="dropdown" class="dropdown-toggle" href="">Produits<b class="caret"></b></a>
-        <ul class="dropdown-menu">
-            <li><a href="{{ url('liste-produit') }}">Liste des produits</a></li>
-            <li><a href="{{ url('categorie-liste') }}">Listes des catégories</a></li>
-            <li><a href="{{ url('create-produit') }}">Ajouter un produit</a></li> 
-            <li><a href="{{ url('create-categorie') }}">Ajouter une catégorie</a></li>   
-        </ul>
-    </li>
 <!--li class="dropdown">
 
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mes Contrats<span class="caret"></span></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('create-contratsClients') }}">Souscrire à un contrat </a></li>
-        <li> <a href="{{ url('list-contratsClients') }}">Mes contrats</a></li>
-    </ul>
+<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mes Contrats<span class="caret"></span></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('create-contratsClients') }}">Souscrire à un contrat </a></li>
+<li> <a href="{{ url('list-contratsClients') }}">Mes contrats</a></li>
+</ul>
 >>>>>>> b572a75d4457f439f644ec770d3ec84ee06b2819
 </li>
 <li class="dropdown">
-    <a  data-toggle="dropdown" class="dropdown-toggle" href="">Modèle de contrats<b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('liste-contrat') }}">Modèle de contrat</a></li>
-        <li><a href="{{ url('create-contrat') }}">Nouveau</a></li>   
-    </ul>
+<a  data-toggle="dropdown" class="dropdown-toggle" href="">Modèle de contrats<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('liste-contrat') }}">Modèle de contrat</a></li>
+<li><a href="{{ url('create-contrat') }}">Nouveau</a></li>   
+</ul>
 <<<<<<< HEAD
 </li>
 <li class="dropdown">
-   <a  data-toggle="dropdown" class="dropdown-toggle" href="">Paramètre<b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('list-roles') }}">Rôles</a></li>
-        <li><a href="{{ url('list-periodicites') }}">Periodicité</a></li> 
-        <li><a href="{{ url('create-contratsClients') }}">Ajouter un contrat amapien</a></li>
-    </ul>
+<a  data-toggle="dropdown" class="dropdown-toggle" href="">Paramètre<b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('list-roles') }}">Rôles</a></li>
+<li><a href="{{ url('list-periodicites') }}">Periodicité</a></li> 
+<li><a href="{{ url('create-contratsClients') }}">Ajouter un contrat amapien</a></li>
+</ul>
 </li> 
 =======
 </li-->
->>>>>>> b572a75d4457f439f644ec770d3ec84ee06b2819
 
 <li class="dropdown">
-    <a  data-toggle="dropdown" class="dropdown-toggle" href="">Paramètres <b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('list-roles') }}">Rôles</a></li>
-        <li><a href="{{ url('list-periodicites') }}">Periodicité</a></li>
-        <li><a href="{{ url('create-contratsClients') }}">Souscrire à un contrat </a></li>
-        <li> <a href="{{ url('list-contratsClients') }}">Mes contrats</a></li>
-        <li><a href="{{ url('liste-contrat') }}">Modèle de contrat</a></li>
-        <li><a href="{{ url('create-contrat') }}">Nouveau</a></li>   
-    </ul>
+<a  data-toggle="dropdown" class="dropdown-toggle" href="">Paramètres <b class="caret"></b></a>
+<ul class="dropdown-menu">
+<li><a href="{{ url('list-roles') }}">Rôles</a></li>
+<li><a href="{{ url('list-periodicites') }}">Periodicité</a></li>
+<li><a href="{{ url('create-contratsClients') }}">Souscrire à un contrat </a></li>
+<li> <a href="{{ url('list-contratsClients') }}">Mes contrats</a></li>
+<li><a href="{{ url('liste-contrat') }}">Modèle de contrat</a></li>
+<li><a href="{{ url('create-contrat') }}">Nouveau</a></li>   
+</ul>
 </li>
