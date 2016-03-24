48a3e3c9bdce5c4063d8434d81ac116229787126
EK <elmahdi.korfed@gmail.com>
2016-03-24 20:22:45 +0100

    maj

--- resources/views/ReferentPlus/Categorie/listcategorie.blade.php
+++ resources/views/ReferentPlus/Categorie/listcategorie.blade.php
@@ -1,14 +1,14 @@
 @extends('template')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
 
-                <div class="panel-body">
-                    <h2>Liste des catégories </h2>
-                    <table class="table  table-bordered">
-                        <thead class="thead-inverse text-center">
+            <div class="panel-body">
+                <h2>Liste des catégories </h2>
+                <table class="table  table-bordered">
+                    <thead class="thead-inverse text-center">
                         <tr >
                             <th>Catégorie</th>
                             <th>Periodicite</th>
@@ -16,8 +16,8 @@
                             <th>Referent</th>
                             <th>Actions</th>
                         </tr>
-                        </thead>
-                        <tbody>
+                    </thead>
+                    <tbody>
                         @foreach ($categories as $key => $row)
                         <tr>
                             <td>
@@ -26,26 +26,26 @@
                             <td>
                                 {{$periodicites[$row->id]->libelle}}
                             </td>
-                             <td>
+                            <td>
                                 {{$producteurs[$row->id]->prenom." ".$producteurs[$row->id]->nom}}
                             </td>
                             <td>
                                 {{$referents[$row->id]->prenom.' '.$referents[$row->id]->nom}}
                             </td>
                             <td>
-                                 <a href="{{ url('categorie-details/'.$row->id) }}" class="btn  btn-info btn-sm">Détails</a>
-                                 @if(session('role')>=3)
-                                 <a href="{{ url('categorie-modifier/'.$row->id) }}" class="btn  btn-info btn-sm">Modifier</a>
-                                 <a href="{{ url('/supprimer-categorie/'.$row->id) }}" class="btn  btn-danger btn-sm confirm">Supprimer</a>
-                                @endif
-                            </td>
-                        </tr>
+                               <a href="{{ url('categorie-details/'.$row->id) }}" class="btn  btn-info btn-sm">Détails</a>
+                               @if(session('role')>=3)
+                               <a href="{{ url('categorie-modifier/'.$row->id) }}" class="btn  btn-info btn-sm">Modifier</a>
+                               <a href="{{ url('/supprimer-categorie/'.$row->id) }}" class="btn  btn-danger btn-sm confirm">Supprimer</a>
+                               @endif
+                           </td>
+                       </tr>
 
-                         @endforeach       
-                        </tbody>
-                        </table>
-    </div>
-    </div>
-        </div>
-    </div>
+                       @endforeach       
+                   </tbody>
+               </table>
+           </div>
+       </div>
+   </div>
+</div>
 @endsection
