622e8122bfa7f8b8729596a8643fc5a8a24ad43c
Kévin <progkeving@gmail.com>
2016-04-05 23:44:57 +0200

    comment

--- app/Http/Controllers/NFCmobile.php
+++ app/Http/Controllers/NFCmobile.php
@@ -7,15 +7,32 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+/*
+* vérifier les routes dans route
+*/
+
 class NFCmobile extends Controller
 {
+	/*@Todo 
+		vérifier dans la bdd si il y a bien l'utilisateur et renvoyer le booléan correspondant
+		ne pas oublier de décommenter le code sur android
+	*/
    public function indentifiants(Request $request){
-        var $bool =  false;
+   	// récupération "email" et "password"
+   	var $email = $request->input("email");
+   	var $password= $request->input("password"); 
+    var $bool =  false;
 
-        return response()->json(['connected' => $bool]);
+    return response()->json(['connected' => $bool]);
    }
 
+   /* @Todo enregistrer la signature dans la feuille de présence associé
+   et renvoyer le boolean
+   */
    public function signature(Request $request){
+   	// récupération "email"de l'utilisateur et "sign" à true 
+   	var $email = $request->input("email");
+   	var $sing  = $request->input("sign"); // <= osef je crois
     var $bool = false; 
     
     return response()->json(['signed'=>$bool])
