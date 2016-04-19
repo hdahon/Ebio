8f264238891f0345586603e47fac397e5bd180d2
cecilerouselot <cecile.cridelich@gmail.com>
2016-04-19 17:19:32 +0200

    Update login.blade.php

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -39,7 +39,7 @@
                                 <div class="col-md-6 col-md-offset-4">
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember"> Remember Me
+                                            <input type="checkbox" name="remember"> Se souvenir de moi
                                         </label>
                                     </div>
                                 </div>
@@ -49,7 +49,7 @@
                                 <div class="col-md-9 col-md-offset-3">
                                     <button type="submit" class="btn btn-primary">Se connecter</button>
                                     <a href="{{url('auth/register')}}" class="btn btn-info">S'inscrire</a>
-                                    <a class="btn btn-default" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+                                    <a class="btn btn-default" href="{{ url('/password/email') }}">Mot de passe oublié</a>
                                 </div>
                             </div>
                         </form>
