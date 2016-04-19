e1da02a5241042b266349d8e7acaa24ae59d0ae7
cecilerouselot <cecile.cridelich@gmail.com>
2016-04-19 17:16:47 +0200

    Update login.blade.php

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -22,14 +22,14 @@
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">E-Mail Address</label>
+                                <label class="col-md-4 control-label">Adresse email</label>
                                 <div class="col-md-6">
                                     <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Password</label>
+                                <label class="col-md-4 control-label">Mot de passe</label>
                                 <div class="col-md-6">
                                     <input type="password" class="form-control" name="password">
                                 </div>
