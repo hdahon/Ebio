0950e956d058f2a0778a790fe1bcf1d5a94a12e9
cecilerouselot <cecile.cridelich@gmail.com>
2016-04-22 12:32:58 +0200

    Update newLivraison.blade.php

--- resources/views/pages/livraisons/newLivraison.blade.php
+++ resources/views/pages/livraisons/newLivraison.blade.php
@@ -11,7 +11,7 @@ Reférent
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
                             <div class="panel panel-default">
-                                <div class="panel-heading">Generer les dates de livraisons</div>
+                                <div class="panel-heading">Génerer les dates de livraisons</div>
                                 <div class="panel-body">
                                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/livraisons/new') }}">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
